d63f55791086085b0b169bfc2076af4eaec7816b
automationdigital01 <123158399+automationdigital01@users.noreply.github.com>
2023-10-19 15:39:31 +0530

    Update feedback_excel.py

--- model/feedback_category_rf/feedback_excel.py
+++ model/feedback_category_rf/feedback_excel.py
@@ -14,7 +14,7 @@ from transformers import pipeline
 
 sentiment_pipeline=pipeline('sentiment-analysis')
 
-rf_loaded=jb.load('model/feedback_category_rf/feedback_model_updated.pkl')
+rf_loaded=jb.load('model/feedback_category_rf/feedback_model.pkl')
 v=jb.load('model/feedback_category_rf/tfidf_vectorizer.pkl')
 
 
